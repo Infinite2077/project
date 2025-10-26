a59b25e18d99dd0e4a1e6261d045cea6d5e7a165
infinite2077 <hrechyshkinvo@gmail.com>
2025-10-26 15:48:48 +0200

    ovovodvovo

--- drawSQL-mysql-export-2025-10-19.sql
+++ drawSQL-mysql-export-2025-10-19.sql
@@ -5,3 +5,11 @@ CREATE TABLE `products`(
     `desc` TEXT NOT NULL,
     `image` TEXT NOT NULL
 );
+CREATE TABLE `comments`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `post_id` INT NOT NULL,
+    `author` VARCHAR(255) NOT NULL,
+    `comment` TEXT NOT NULL
+);
+ALTER TABLE
+    `comments` ADD CONSTRAINT `comments_post_id_foreign` FOREIGN KEY(`post_id`) REFERENCES `products`(`id`);
--- index.js
+++ index.js
@@ -63,11 +63,26 @@ app.get("/", (req,res)=>{
 
 app.get("/post/:id", (req,res)=>{
     let postId = req.params.id
-    if(!ads[postId]){
-        res.render("notFound")
-        return
-    }
-    res.render("post", {product: ads[postId]})
+    db.query(`SELECT p.*, c.id as commentId, c.author, c.comment FROM products as p LEFT JOIN comments as c ON p.id = postId WHERE p.id = ?`, postId, (err, result)=>{
+        if(err || result.length == 0){
+            console.log(err)
+            return res.status(404).render("notFound")
+        }
+        let product = result[0]
+        console.log(result)
+        product.image = JSON.parse(product.image)
+        res.status(200).render("post", {product})
+    }) 
+})
+
+app.post("/comment", (req, res)=>{
+    let data = req.body
+    console.log(data)
+    db.query(`INSERT INTO comments SET ?`, data, (err, result)=>{
+        if(err) res.status(500)
+        res.end()
+    })
+    
 })
 
 app.post("/add", upload.fields([{name: "image"}]), (req, res)=>{
